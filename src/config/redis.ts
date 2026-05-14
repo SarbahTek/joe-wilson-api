fe7c1404dfdb527eae990820c77f8ef51b6fa505
Albert <albertodoom193@gmail.com>
2026-05-14 21:30:34 +0000

    redis file update

--- src/config/redis.ts
+++ src/config/redis.ts
@@ -1,10 +1,10 @@
 import Redis from 'ioredis';
 import { env } from './env';
 
-const usetls = env.REDIS_URL.startsWith('redis://')
+const useTLS = env.REDIS_URL.startsWith('redis://')
 
 export const redis = new Redis(env.REDIS_URL, {
-  ...(useTLS ? {tls : {}} : {})
+  ...(useTLS ? {tls : {}} : {}),
   maxRetriesPerRequest: null,
   lazyConnect: true,
 });
