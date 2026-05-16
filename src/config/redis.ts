7e2414fce68bd2772a64108e43e0711ca0dc0baa
Albert <albertodoom193@gmail.com>
2026-05-16 01:06:52 +0000

    redis fix

--- src/config/redis.ts
+++ src/config/redis.ts
@@ -1,13 +1,21 @@
 import Redis from 'ioredis';
 import { env } from './env';
+import { logger } from './logger';
 
-const useTLS = env.REDIS_URL.startsWith('redis://')
+const isTLS = env.REDIS_URL?.startsWith('rediss://');
 
-export const redis = new Redis(env.REDIS_URL, {
-  ...(useTLS ? {tls : {}} : {}),
-  maxRetriesPerRequest: null,
-  lazyConnect: true,
+export const redis = new Redis(env.REDIS_URL as string, {
+  ...(isTLS ? { tls: {} } : {}),
+
+  maxRetriesPerRequest: 3,
+  enableReadyCheck: true,
+  lazyConnect: false, 
 });
 
+redis.on('connect', () => {
+  logger.info('✅ Redis connected');
+});
 
-
+redis.on('error', (err) => {
+  logger.error(err, '❌ Redis error');
+});
